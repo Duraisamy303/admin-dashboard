400224cf955c7da6c2e9f364fbe49bc8468c4fd2
Duraisamy <duraisamy.p@sanorac.com>
2024-07-26 14:43:18 +0530

    low stock pagination modify old

--- pages/lowStock.tsx
+++ pages/lowStock.tsx
@@ -1,148 +1,121 @@
+import IconTrashLines from '@/components/Icon/IconTrashLines';
 import { LOW_STOCK_LIST } from '@/query/product';
-import { useLazyQuery, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import { DataTable, DataTableSortStatus } from 'mantine-datatable';
+import moment from 'moment';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CommonLoader from './elements/commonLoader';
-import { DataTable } from 'mantine-datatable';
 import PrivateRouter from '@/components/Layouts/PrivateRouter';
-import IconArrowBackward from '@/components/Icon/IconArrowBackward';
-import IconArrowForward from '@/components/Icon/IconArrowForward';
 
 const LowStock = () => {
     const router = useRouter();
-    const PAGE_SIZE = 20;
 
-    const [recordsData, setRecordsData] = useState([]);
-    const [startCursor, setStartCursor] = useState(null);
-    const [endCursor, setEndCursor] = useState(null);
-    const [hasNextPage, setHasNextPage] = useState(false);
-    const [hasPreviousPage, setHasPreviousPage] = useState(false);
-    const [search, setSearch] = useState('');
-
-    const { loading: getLoading, refetch: productSearchRefetch } = useQuery(LOW_STOCK_LIST, {
+    const {
+        data: lowStockList,
+        loading: getLoading,
+        refetch: productSearchRefetch,
+    } = useQuery(LOW_STOCK_LIST, {
         variables: {
             channel: 'india-channel',
-            first: PAGE_SIZE,
+            first: 500,
             after: null,
             filter: {
                 categories: [],
                 stockAvailability: 'OUT_OF_STOCK',
             },
         },
-        onCompleted: (data) => {
-            const products = data?.products?.edges;
-            const pageInfo = data?.products?.pageInfo;
-            setRecordsData(tableFormat(products));
-            setStartCursor(pageInfo?.startCursor || null);
-            setEndCursor(pageInfo?.endCursor || null);
-            setHasNextPage(pageInfo?.hasNextPage || false);
-            setHasPreviousPage(pageInfo?.hasPreviousPage || false);
-        },
     });
 
-    const [fetchNextPage] = useLazyQuery(LOW_STOCK_LIST, {
-        onCompleted: (data) => {
-            const products = data?.products?.edges;
-            const pageInfo = data?.products?.pageInfo;
-            setRecordsData(tableFormat(products));
-            setStartCursor(pageInfo?.startCursor || null);
-            setEndCursor(pageInfo?.endCursor || null);
-            setHasNextPage(pageInfo?.hasNextPage || false);
-            setHasPreviousPage(pageInfo?.hasPreviousPage || false);
-        },
+    const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
+        columnAccessor: 'id',
+        direction: 'asc',
     });
 
-    const [fetchPreviousPage] = useLazyQuery(LOW_STOCK_LIST, {
-        onCompleted: (data) => {
-            const products = data?.products?.edges;
-            const pageInfo = data?.products?.pageInfo;
-            setRecordsData(tableFormat(products));
-            setStartCursor(pageInfo?.startCursor || null);
-            setEndCursor(pageInfo?.endCursor || null);
-            setHasNextPage(pageInfo?.hasNextPage || false);
-            setHasPreviousPage(pageInfo?.hasPreviousPage || false);
-        },
-    });
+    const PAGE_SIZES = [10, 20, 30];
+    const [page, setPage] = useState(1);
+    const [pageSize, setPageSize] = useState(10);
+    const [lowStockData, setLowStockData] = useState([]);
+    const [initialData, setInitialData] = useState([]);
+    const [search, setSearch] = useState('');
 
-    const tableFormat = (products) => {
-        return products.map((product) => ({
-            name: product?.node?.name,
-            id: product?.node?.id,
-        }));
-    };
+    useEffect(() => {
+        if (lowStockList?.products?.edges?.length > 0) {
+            const newData = lowStockList?.products?.edges?.map((item: any) => {
+                return {
+                    ...item.node,
+                    name: item?.node?.name || '',
+                    id: item?.node?.id,
+                };
+            });
 
-    const handleNextPage = () => {
-        fetchNextPage({
-            variables: {
-                channel: 'india-channel',
-                first: PAGE_SIZE,
-                after: endCursor,
-                filter: {
-                    categories: [],
-                    stockAvailability: 'OUT_OF_STOCK',
-                },
-            },
-        });
-    };
+            setLowStockData(newData);
+        }
+    }, [lowStockList]);
 
-    const handlePreviousPage = () => {
-        fetchPreviousPage({
-            variables: {
-                channel: 'india-channel',
-                last: PAGE_SIZE,
-                before: startCursor,
-                filter: {
-                    categories: [],
-                    stockAvailability: 'OUT_OF_STOCK',
-                },
-            },
-        });
-    };
+    useEffect(() => {
+        const from = (page - 1) * pageSize;
+        const to = from + pageSize;
+        setInitialData([...lowStockData.slice(from, to)]);
+    }, [page, pageSize, lowStockData]);
+
+    useEffect(() => {
+        if (search === '') {
+            // If search input is cleared, show all data
+            const from = (page - 1) * pageSize;
+            const to = from + pageSize;
+            setInitialData([...lowStockData.slice(from, to)]);
+        } else {
+            // If there is a search term, filter the data
+            setInitialData(() => {
+                return lowStockData.filter((item: any) => {
+                    return item?.name?.toLowerCase().includes(search.toLowerCase());
+                });
+            });
+        }
+    }, [search, page, pageSize, lowStockData]);
 
     return (
-        <div>
+        <div className="">
             <div className="panel mb-5 flex flex-col justify-between gap-5 md:flex-row md:items-center">
                 <h5 className="text-lg font-semibold dark:text-white-light">Out Of Stocks</h5>
-                <input
-                    type="text"
-                    className="form-input mb-3 mr-2 w-full md:mb-0 md:w-auto"
-                    placeholder="Search..."
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                />
+                <input type="text" className="form-input  mb-3 mr-2 w-full md:mb-0 md:w-auto" placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value)} />
             </div>
             <div className="datatables">
                 {getLoading ? (
                     <CommonLoader />
                 ) : (
-                    <>
-                        <DataTable
-                            className="table-hover whitespace-nowrap"
-                            records={recordsData}
-                            columns={[
-                                {
-                                    accessor: 'name',
-                                    sortable: true,
-                                    title: 'Product Name',
-                                    render: (row) => (
-                                        <div className="cursor-pointer" onClick={() => router.push(`/apps/product/edit?id=${row.id}`)}>
-                                            {row.name}
-                                        </div>
-                                    ),
-                                },
-                            ]}
-                            highlightOnHover
-                            minHeight={200}
-                        />
-                        <div className="mt-5 flex justify-end gap-3">
-                            <button disabled={!hasPreviousPage} onClick={handlePreviousPage} className={`btn ${!hasPreviousPage ? 'btn-disabled' : 'btn-primary'}`}>
-                                <IconArrowBackward />
-                            </button>
-                            <button disabled={!hasNextPage} onClick={handleNextPage} className={`btn ${!hasNextPage ? 'btn-disabled' : 'btn-primary'}`}>
-                                <IconArrowForward />
-                            </button>
-                        </div>
-                    </>
+                    <DataTable
+                        className="table-hover whitespace-nowrap"
+                        records={initialData}
+                        columns={[
+                            {
+                                accessor: 'name',
+                                sortable: true,
+                                title: 'Product Name',
+                                render: (row) => (
+                                    <div className='cursor-pointer' onClick={() => router.push(`/apps/product/edit?id=${row.id}`)}>
+                                        {row.name}
+                                    </div>
+                                ),
+                            },
+                        ]}
+                        highlightOnHover
+                        totalRecords={lowStockData.length}
+                        recordsPerPage={pageSize}
+                        page={page}
+                        onPageChange={(p) => setPage(p)}
+                        recordsPerPageOptions={PAGE_SIZES}
+                        onRecordsPerPageChange={setPageSize}
+                        sortStatus={null}
+                        onSortStatusChange={null}
+                        selectedRecords={null}
+                        onSelectedRecordsChange={(selectedRecords) => {
+                            // setSelectedRecords(selectedRecords);
+                        }}
+                        minHeight={200}
+                        paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
+                    />
                 )}
             </div>
         </div>
